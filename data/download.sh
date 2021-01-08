cee0f45b895ec0a57fcee9ea04eb0429bd8921b0
Terry-Ma <rssmyq@aliyun.com>
2021-01-08 02:27:45 +0000

    add data

---
+++ data/download.sh
@@ -0,0 +1,5 @@
+wget https://mind201910small.blob.core.windows.net/release/MINDlarge_train.zip &
+wget https://mind201910small.blob.core.windows.net/release/MINDlarge_test.zip &
+wget https://mind201910small.blob.core.windows.net/release/MINDlarge_dev.zip &
+wget https://mind201910small.blob.core.windows.net/release/MINDsmall_dev.zip &
+wget https://mind201910small.blob.core.windows.net/release/MINDsmall_train.zip &
